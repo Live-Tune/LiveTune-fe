bfe698d2e167a329e1f1fd19a12c876cc87f9dc2
Moderator11 <p7925221@gmail.com>
2025-05-15 20:17:39 +0900

    style: Change BackgroundGradient code placement
    - BackgroundGradient code moved to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import styled from "styled-components";
 import "./App.css";
-import logo from "./assets/LiveTuneLogo.png";
 import Router from "./shared/Router";
 
 function App() {
   return (
     <>
-      <Router />
+      <BackgroundGradient>
+        <Router />
+      </BackgroundGradient>
     </>
   );
 }
+
+const BackgroundGradient = styled.div`
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  background: linear-gradient(90deg, #0097b1, #7ed957);
+`;
 
 export default App;
--- src/pages/TemporaryMainPage.jsx
+++ src/pages/TemporaryMainPage.jsx
@@ -4,12 +4,10 @@ import LoginPanel from "../components/dev1/LoginPanel";
 
 function TemporaryMainPage() {
   return (
-    <BackgroundGradient>
-      <PanelWrapper>
-        <LiveTuneLogo src={logo} alt="LiveTune Logo" />
-        <LoginPanel />
-      </PanelWrapper>
-    </BackgroundGradient>
+    <PanelWrapper>
+      <LiveTuneLogo src={logo} alt="LiveTune Logo" />
+      <LoginPanel />
+    </PanelWrapper>
   );
 }
 
@@ -21,13 +19,6 @@ const PanelWrapper = styled.div`
 
 const LiveTuneLogo = styled.img`
   filter: hue-rotate(140deg) saturate(200%) brightness(30%);
-`;
-
-const BackgroundGradient = styled.div`
-  width: 100%;
-  height: 100%;
-  min-height: 100vh;
-  background: linear-gradient(90deg, #0097b1, #7ed957);
 `;
 
 export default TemporaryMainPage;
